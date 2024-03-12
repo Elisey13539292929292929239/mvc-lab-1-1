c9644547c4da75f8c21fda720f968b73c1934244
Елисей Угримов <elisejugrimov@MacBook-Air-Elisej.local>
2024-03-12 09:47:05 +0100

    Добавлено задание 1 в папку zad1

---
+++ student.js
@@ -0,0 +1,5 @@
+// Замените 'Your Name' на ваше имя и фамилию и 'YourStudentID' на ваш номер студенческого билета
+const fullName = "Yelysei";
+const studentId = "45923";
+
+console.log(`My name is ${fullName}. My student ID is ${studentId}`);
---
+++ zad1/student.js
@@ -0,0 +1,3 @@
+const fullName = "Yelysei";
+const studentId = "45923";
+console.log(`My name is ${fullName}. My student ID is ${studentId}`);
