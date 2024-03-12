dca04342da3c7a0bc36f7af035d3267631ac1b87
Елисей Угримов <elisejugrimov@MacBook-Air-Elisej.local>
2024-03-12 10:17:54 +0100

    Добавлено задание 1 в папку zad1

---
+++ zad2/student_zad2.js
@@ -0,0 +1,13 @@
+const FULLNAME = "Yelysei";
+const STUDENT_ID = "45923";
+
+function getStudentFullName() {
+    return FULLNAME;
+}
+
+function getStudentId() {
+    return STUDENT_ID;
+}
+
+console.log(`My name is ${getStudentFullName()}. My student ID is ${getStudentId()}`);
+
