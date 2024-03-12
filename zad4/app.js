085ba01b77f118b5c71ceb6e642d702108212d5a
Elisey13539292929292929239 <135936150+Elisey13539292929292929239@users.noreply.github.com>
2024-03-12 10:36:48 +0100

    Update app.js

--- zad4/app.js
+++ zad4/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const student = require('./student'); 
+const student = require('./student_zad4'); 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
